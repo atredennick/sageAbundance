4e99685703d59a94d257430a80771744116a6873
Andrew Tredennick <atredenn@gmail.com>
2015-09-02 14:47:53 -0600

    takes out chain manual setting

--- scripts/run_subset.R
+++ scripts/run_subset.R
@@ -26,7 +26,7 @@ args <- commandArgs(trailingOnly = F)
 myargument <- args[length(args)]
 myargument <- sub("-","",myargument)
 chain_id <- as.numeric(myargument)
-chain_id <- 1
+
 
 ####
 ####  Get data
