6c11065d9038257f619b906129c2035d4bba5e8a
Andrew Tredennick <atredenn@gmail.com>
2015-09-11 15:07:46 -0600

    updates iter max for traceplots

--- scripts/traceplots.R
+++ scripts/traceplots.R
@@ -1,24 +1,25 @@
 
 setwd("/Users/atredenn/Desktop/")
+maxiter <- 5
 longchain1 <- readRDS("POIS_iterchunk1_chain2.RDS")
-for(i in 2:3){
+for(i in 2:maxiter){
   longchain1 <- rbind(longchain1, readRDS(paste0("POIS_iterchunk",i,"_chain2.RDS")))
 }
 
 longchain2 <- readRDS("POIS_iterchunk1_chain3.RDS")
-for(i in 2:3){
+for(i in 2:maxiter){
   longchain2 <- rbind(longchain2, readRDS(paste0("POIS_iterchunk",i,"_chain3.RDS")))
 }
 
 longchain3 <- readRDS("POIS_iterchunk1_chain1.RDS")
-for(i in 2:3){
+for(i in 2:maxiter){
   longchain3 <- rbind(longchain3, readRDS(paste0("POIS_iterchunk",i,"_chain1.RDS")))
 }
 
 
 all.params <- unique(longchain1$Parameter)
-iters <- c(100:600)
-iters <- iters[-which(iters %in% c(201:205,401:405))]
+iters <- c(100:1000)
+iters <- iters[-which(iters %in% c(201:205,401:405,601:605,801:805))]
 
 pdf("test.pdf", width = 8.5, height = 11)
 par(mfrow=c(6,3))
