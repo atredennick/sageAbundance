bd75ae740724600a6491c246b8f4a1a12480fb21
Andrew Tredennick <atredenn@gmail.com>
2015-12-08 11:52:06 -0700

    updates spatial equilibrium sims to run by model

--- scripts/sim_equilibrium_forecasts.R
+++ scripts/sim_equilibrium_forecasts.R
@@ -22,6 +22,8 @@ rm(list=ls())
 datapath <- "../data/"
 knotpath <- "../results/"
 
+global_time_steps <- 100
+global_burnin <- 50
 
 
 ####
@@ -65,6 +67,16 @@ growD$CoverLag <- round(growD$CoverLag,0) # round for count-like data
 # Climate data
 climD <- read.csv(paste0(datapath,"/climate/DAYMET/FormattedClimate_WY_SA1.csv"))
 
+# Climate projections
+ppt_projs <- subset(read.csv("../data/precipitation_projections_bymodel.csv"), 
+                    scenario!="rcp26" & season=="fall2spr")
+temp_projs <- subset(read.csv("../data/temperature_projections_bymodel.csv"),
+                     scenario!="rcp26" & season=="spring")
+projC<-data.frame("scenario"=c("rcp45","rcp60","rcp85"),
+                  "deltaPpt"=1+ppt_projs$change,
+                  "deltaTspr"=temp_projs$change,
+                  "model"=ppt_projs$model)
+
 # Knots and MCMC results
 load(paste0(knotpath,"Knot_cell_distances_subset.Rdata"))
 K <- K.data$K
@@ -96,6 +108,7 @@ sim_sage <- function(init_cover_vector, scaled_clim_matrix,
   cover_matrix <- matrix(NA,nrow=time_steps,ncol=length(init_cover_vector))
   cover_matrix[1,] <- init_cover_vector
   X <- scaled_climate_matrix
+  pb <- txtProgressBar(min=2, max=time_steps, char="+", style=3, width=65)
   for(t in 2:time_steps){
     ## Growth Process
     Xtmp <- X[sample(c(1:nrow(X)), 1),]
@@ -103,50 +116,116 @@ sim_sage <- function(init_cover_vector, scaled_clim_matrix,
     dens.dep[which(dens.dep==-Inf)] <- 0
     tmp.mu <- int_mu + dens.dep + sum(betas*Xtmp)
     tmp.mu <- exp(tmp.mu + eta)
-    tmp.out <- rpois(ncol(ex.mat), lambda = tmp.mu)
+    tmp.out <- rpois(ncol(cover_matrix), lambda = tmp.mu)
     
     ## Colonization Process
-    zeros <- which(ex.mat[t-1,]==0)
+    zeros <- which(cover_matrix[t-1,]==0)
     colonizers <- rbinom(length(zeros), size = 1, antilogit(col.intercept))
     colonizer.cover <- colonizers*avg_new_cover
     tmp.out[zeros] <- colonizer.cover
-    ex.mat[t,] <- tmp.out
+    cover_matrix[t,] <- tmp.out
+    setTxtProgressBar(pb, t)
   } # end simulation loop
-  return(ex.mat)
+  return(cover_matrix)
 } # end simulation function
 
 
 
 ####
-####  Run equilibrium population simulation ------------------------------------
+####  Set Up Parameter List for Simulations
 ####
-time.steps <- 2000
-burn.in <- 100
 mean_params <- ddply(outs, .(Parameter), summarise,
                      value = mean(value))
 alphas <- mean_params[grep("alpha", mean_params$Parameter),"value"]
 betas <- mean_params[grep("beta", mean_params$Parameter),"value"][2:6]
 eta <- K%*%alphas
-pixels <- nrow(subset(growD, Year==1985))
-ex.mat <- matrix(NA,nrow=time.steps,ncol=pixels)
-ex.mat[1,] <- 1
-clim_sim <- climD[climD$year %in% unique(growD$Year),]
-X_sim = clim_sim[,c("pptLag", "ppt1", "ppt2", "TmeanSpr1", "TmeanSpr2")]
-X_sim = scale(X_sim, center = TRUE, scale = TRUE)
+beta_mu <- mean_params[mean_params$Parameter=="beta_mu","value"]
+int_mu <- mean_params[mean_params$Parameter=="int_mu","value"]
+params <- list(int_mu=int_mu, beta_mu=beta_mu, betas=betas, eta=eta)
 
-for(t in 2:time.steps){
-  Xtmp <- X_sim[sample(c(1:nrow(X_sim)), 1),]
-  dens.dep <- mean_params[mean_params$Parameter=="beta_mu","value"]*log(ex.mat[t-1,])
-  tmp.mu <- mean_params[mean_params$Parameter=="int_mu","value"] + dens.dep + sum(betas*Xtmp)
-  tmp.mu <- exp(tmp.mu + eta)
-  tmp.out <- rpois(ncol(ex.mat), lambda = tmp.mu)
+
+
+####
+####  Run Equilibrium Population Simulation
+####
+clim_vars <- c("pptLag", "ppt1", "ppt2", "TmeanSpr1", "TmeanSpr2")
+init_cover_vector <- rep(1, times=nrow(subset(growD, Year==1985)))
+Xtmp <- climD[climD$year %in% unique(growD$Year),clim_vars]
+scaled_climate_matrix <- scale(Xtmp, center = TRUE, scale = TRUE)
+avg_new_cover <- avg.new.cover
+time_steps <- global_time_steps
+equil_simulation <- sim_sage(init_cover_vector, scaled_clim_matrix, 
+                             avg_new_cover, params, time_steps)
+equil_results <- colMeans(equil_simulation[(burn_in+1):time_steps,])
+
+
+
+####
+####  Run Climate Change Simulations by Model
+####
+obs_clim<-climD[climD$year %in% unique(growD$Year),clim_vars]
+clim_avg <- apply(X = obs_clim, MARGIN = 2, FUN = mean)
+clim_sd <- apply(X = obs_clim, MARGIN = 2, FUN = sd)
+
+sim_matrix <- matrix(NA, nrow = nrow(projC), ncol = nrow(subset(growD, Year==1985)))
+time_steps <- global_time_steps
+burn_in <- global_burnin
+for(i in 1:nrow(projC)){
+  p.climD <- obs_clim
+  p.climD[,c(1:3)]<-obs_clim[,c(1:3)]*matrix(projC[i,"deltaPpt"],dim(climD)[1],3)
+  p.climD[,c(4:5)]<-p.climD[,c(4:5)]+matrix(projC[i,"deltaTspr"],dim(climD)[1],2)
+  X_sim = p.climD[,clim_vars]
   
-  #Colonization
-  zeros <- which(ex.mat[t-1,]==0)
-  colonizers <- rbinom(length(zeros), size = 1, antilogit(col.intercept))
-  colonizer.cover <- colonizers*avg.new.cover
-  tmp.out[zeros] <- colonizer.cover
-  
-  ex.mat[t,] <- tmp.out
+  # Now scale based on perturbed or regular data, depending on scenario
+  X_sim["pptLag"] <- (X_sim["pptLag"] - clim_avg["pptLag"])/clim_sd["pptLag"]
+  X_sim["ppt1"] <- (X_sim["ppt1"] - clim_avg["ppt1"])/clim_sd["ppt1"]
+  X_sim["ppt2"] <- (X_sim["ppt2"] - clim_avg["ppt2"])/clim_sd["ppt2"]
+  X_sim["TmeanSpr1"] <- (X_sim["TmeanSpr1"] - clim_avg["TmeanSpr1"])/clim_sd["TmeanSpr1"]
+  X_sim["TmeanSpr2"] <- (X_sim["TmeanSpr2"] - clim_avg["TmeanSpr2"])/clim_sd["TmeanSpr2"]
+  scaled_climate_matrix <- X_sim
+  current_simulation <- sim_sage(init_cover_vector, scaled_clim_matrix, 
+                               avg_new_cover, params, time_steps)
+  current_results <- colMeans(current_simulation[(burn_in+1):time_steps,])
+  sim_matrix[i,] <- current_results
+  print(i)
 }
+
+
+
+####
+####  Average Simulation Results by RCP Scenario
+####
+rcps <- c("rcp45", "rcp60", "rcp85")
+rcp_matrix <- matrix(NA, nrow = length(rcps), ncol=ncol(sim_matrix))
+for(i in 1:length(rcps)){
+  ids <- which(projC$scenario==rcps[i])
+  rcp_matrix[i,] <- colMeans(sim_matrix[ids,])
+}
+
+
+
+####
+####  Combine Output and Make Spatil Plot
+####
+proj.equil <- data.frame(Lon=subset(growD, Year==1985)$Lon, 
+                         Lat=subset(growD, Year==1985)$Lat,
+                         CURRENT=equil_results,
+                         RCP45=rcp_matrix[1,],
+                         RCP60=rcp_matrix[2,],
+                         RCP85=rcp_matrix[3,])
+proj.equil2 <- melt(proj.equil, id.vars = c("Lon", "Lat"))
+
+# png("../results/climchange_small.png", width = 6, height=4.5, units = "in", res=150)
+ggplot(proj.equil2, aes(x=Lon, y=Lat))+
+  geom_raster(aes(z=value, fill=value))+
+  scale_fill_gradientn(colours=myPalette(200), name="% Cover")+
+  facet_wrap("variable", ncol=1)+
+  coord_equal()+
+  tmp.theme+
+  theme(strip.background=element_rect(fill="white"))
+ggsave("../results/clim_change_mean_spatial.png", height=8, width=4)
+
+
+
+
 
