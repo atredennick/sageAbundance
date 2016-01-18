ea70156b27c9693808c4187e9f5fb69ea95604bc
Andrew Tredennick <atredenn@gmail.com>
2016-01-18 12:41:32 -0700

    updates ms with Collin comments and random year effects; sourcing script working; submission-ready

--- scripts/plot_projected_weather.R
+++ scripts/plot_projected_weather.R
@@ -82,26 +82,26 @@ dev.off()
 ####
 ####  Compare weather hindcasts to average weather at site ---------------------
 ####
-obs_weather <- read.csv("../data/FormattedClimate_WY_SA1.csv")
-obs_weather <- melt(obs_weather, id.vars = "year")
-obs_ppt <- subset(obs_weather, variable=="ppt2")
- 
-model_weather <- subset(model_weather, Year < 2014 & Year > 1979)
-precip_compare <- data.frame(year = model_weather$Year, 
-                             precip = c(obs_ppt$value, model_weather$value),
-                             value_source = rep(c("observed", "modeled"), each=34))
-ggplot(precip_compare)+
-  geom_boxplot(aes(x=value_source, y=precip))
-
-
-obs_temp <- subset(obs_weather, variable=="TmeanSpr2")
-model_weather <- ddply(temp_agg, .(Year, season), summarise,
-                       value = mean(avg_value))
-model_weather <- subset(model_weather, Year < 2014 & Year > 1979)
-temp_compare <- data.frame(year = model_weather$Year, 
-                             temp = c(obs_temp$value, model_weather$value),
-                             value_source = rep(c("observed", "modeled"), each=34))
-ggplot(temp_compare)+
-  geom_boxplot(aes(x=value_source, y=temp))
+# obs_weather <- read.csv("../data/FormattedClimate_WY_SA1.csv")
+# obs_weather <- melt(obs_weather, id.vars = "year")
+# obs_ppt <- subset(obs_weather, variable=="ppt2")
+#  
+# model_weather <- subset(model_weather, Year < 2014 & Year > 1979)
+# precip_compare <- data.frame(year = model_weather$Year, 
+#                              precip = c(obs_ppt$value, model_weather$value),
+#                              value_source = rep(c("observed", "modeled"), each=34))
+# ggplot(precip_compare)+
+#   geom_boxplot(aes(x=value_source, y=precip))
+# 
+# 
+# obs_temp <- subset(obs_weather, variable=="TmeanSpr2")
+# model_weather <- ddply(temp_agg, .(Year, season), summarise,
+#                        value = mean(avg_value))
+# model_weather <- subset(model_weather, Year < 2014 & Year > 1979)
+# temp_compare <- data.frame(year = model_weather$Year, 
+#                              temp = c(obs_temp$value, model_weather$value),
+#                              value_source = rep(c("observed", "modeled"), each=34))
+# ggplot(temp_compare)+
+#   geom_boxplot(aes(x=value_source, y=temp))
 
 
